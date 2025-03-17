c81e782e73383fd12f3c31c1acb636ac7d70eb92
yebin-jeong <2022112390@dgu.ac.kr>
2025-03-17 20:45:31 +0900

    :sparkles: feat: #6 홈 배너 링크 컴포넌트 생성

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [tailwindcss()],
   build: {
     rollupOptions: {
       input: {
-        index: "index.html", // 기본 index.html
+        index: 'index.html', // 기본 index.html
+        homeBanner: '/src/components/banner/home-banner.html',
       },
     },
   },
-  appType: "mpa", // fallback 사용안함
+  appType: 'mpa', // fallback 사용안함
   server: {
     // open: 'src/pages/main/index.html', // 서버 시작 시 브라우저에서 지정페이지 자동으로 열기
   },
