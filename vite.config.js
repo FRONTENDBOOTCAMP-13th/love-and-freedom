540390ea6ee32f864533f214eb025dcfb8a7350a
yebin-jeong <2022112390@dgu.ac.kr>
2025-03-18 09:15:52 +0900

    :lion: chore: #7 이미지 파일 업로드

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [tailwindcss()],
   build: {
     rollupOptions: {
       input: {
-        index: "index.html", // 기본 index.html
+        index: 'index.html', // 기본 index.html
+        chaBanner: '/src/components/banner/character-banner.html',
       },
     },
   },
-  appType: "mpa", // fallback 사용안함
+  appType: 'mpa', // fallback 사용안함
   server: {
     // open: 'src/pages/main/index.html', // 서버 시작 시 브라우저에서 지정페이지 자동으로 열기
   },
